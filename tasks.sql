c1928594a1ed416fde3c63019a79b6a77fa578c0
hikmahnlmm <hikmah.nurlailimaulida32@gmail.com>
2025-06-14 15:16:23 +0700

    menambahkan file sql

---
+++ tasks.sql
@@ -0,0 +1,3 @@
+INSERT INTO `tasks`(`id`, `title`, `status`) VALUES (1, 'belajar php', 'selesai');
+INSERT INTO `tasks`(`id`, `title`, `status`) VALUES (2, 'kerjakan tugas UX', 'selesai');
+INSERT INTO `tasks`(`id`, `title`, `status`) VALUES (4, 'Belajar excel', 'selesai');
